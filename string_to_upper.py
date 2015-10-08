588004e263a6afa5a00590b811c485ddc6e21bf8
pawelkrysiak <pawelkrysiak@gmail.com>
2015-10-08 21:13:30 +0100

    regex solution

--- string_to_upper.py
+++ string_to_upper.py
@@ -1,3 +1,9 @@
 
+import re
+
+def repl_func(m):
+    """process regular expression match groups for word upper-casing problem"""
+    return m.group(1) + m.group(2).upper()
+
 def string_to_upper(string):
-	return string.title()
+	return re.sub("(^|\s)(\S)", repl_func, string)
